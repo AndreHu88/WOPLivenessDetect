7c3f509e6283493b22bbb566147549509d90219b
jack <229376483@qq.com>
2023-10-27 18:30:00 +0800

    first commit

--- Example/Pods/Target Support Files/Pods-WOPLivenessDetect_Example/Pods-WOPLivenessDetect_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-WOPLivenessDetect_Example/Pods-WOPLivenessDetect_Example-frameworks.sh
@@ -176,10 +176,24 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/WOPLivenessDetect/WOPLivenessDetect.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/GTMSessionFetcher/GTMSessionFetcher.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/GoogleDataTransport/GoogleDataTransport.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/GoogleToolboxForMac/GoogleToolboxForMac.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Masonry/Masonry.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PromisesObjC/FBLPromises.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Protobuf/Protobuf.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/WOPLivenessDetect/WOPLivenessDetect.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/GTMSessionFetcher/GTMSessionFetcher.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/GoogleDataTransport/GoogleDataTransport.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/GoogleToolboxForMac/GoogleToolboxForMac.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Masonry/Masonry.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PromisesObjC/FBLPromises.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Protobuf/Protobuf.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
--- Example/Pods/Target Support Files/WOPLivenessDetect/WOPLivenessDetect-umbrella.h
+++ Example/Pods/Target Support Files/WOPLivenessDetect/WOPLivenessDetect-umbrella.h
@@ -10,6 +10,14 @@
 #endif
 #endif
 
+#import "WOPLivenessDetector.h"
+#import "WOPLivenessDetectTaskDelegate.h"
+#import "WOPLivenessDetectTaskFace.h"
+#import "WOPLivenessDetectTaskMouthOpen.h"
+#import "WOPLivenessDetectTaskShakeHead.h"
+#import "WOPLivenessDetectTaskSmile.h"
+#import "WOPLivenessDetectUtils.h"
+#import "WOPLivenessDetectViewController.h"
 
 FOUNDATION_EXPORT double WOPLivenessDetectVersionNumber;
 FOUNDATION_EXPORT const unsigned char WOPLivenessDetectVersionString[];
